0661d4e586a70219a44efa6751138dcc88e0e0bb
vickihi <vickihi2000@gmail.com>
2020-01-29 14:14:44 -0500

    Update FormReturnMovies.Designer.cs

--- MovieRental/FormReturnMovies.Designer.cs
+++ MovieRental/FormReturnMovies.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace MovieRental
+namespace MovieRental
 {
     partial class FormReturnMovies
     {
@@ -28,12 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.grpBorrowedMovies = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.grpUserInformation = new System.Windows.Forms.GroupBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblUserEmail = new System.Windows.Forms.Label();
+            this.txtUserEmail = new System.Windows.Forms.TextBox();
+            this.lblReturnMovies = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.borrowMoviesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.returnMoviesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageUsersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageMoviesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.grpBorrowedMovies.SuspendLayout();
+            this.grpUserInformation.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpBorrowedMovies
+            // 
+            this.grpBorrowedMovies.Controls.Add(this.btnReturn);
+            this.grpBorrowedMovies.Controls.Add(this.tableLayoutPanel1);
+            this.grpBorrowedMovies.Location = new System.Drawing.Point(54, 203);
+            this.grpBorrowedMovies.Margin = new System.Windows.Forms.Padding(2);
+            this.grpBorrowedMovies.Name = "grpBorrowedMovies";
+            this.grpBorrowedMovies.Padding = new System.Windows.Forms.Padding(2);
+            this.grpBorrowedMovies.Size = new System.Drawing.Size(458, 178);
+            this.grpBorrowedMovies.TabIndex = 13;
+            this.grpBorrowedMovies.TabStop = false;
+            this.grpBorrowedMovies.Text = "Borrowed Movies";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 6;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(8, 34);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(434, 81);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // grpUserInformation
+            // 
+            this.grpUserInformation.Controls.Add(this.btnSearch);
+            this.grpUserInformation.Controls.Add(this.lblUserEmail);
+            this.grpUserInformation.Controls.Add(this.txtUserEmail);
+            this.grpUserInformation.Location = new System.Drawing.Point(54, 104);
+            this.grpUserInformation.Margin = new System.Windows.Forms.Padding(2);
+            this.grpUserInformation.Name = "grpUserInformation";
+            this.grpUserInformation.Padding = new System.Windows.Forms.Padding(2);
+            this.grpUserInformation.Size = new System.Drawing.Size(458, 69);
+            this.grpUserInformation.TabIndex = 11;
+            this.grpUserInformation.TabStop = false;
+            this.grpUserInformation.Text = "User Information";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(374, 27);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(56, 21);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // lblUserEmail
+            // 
+            this.lblUserEmail.AutoSize = true;
+            this.lblUserEmail.Location = new System.Drawing.Point(5, 32);
+            this.lblUserEmail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblUserEmail.Name = "lblUserEmail";
+            this.lblUserEmail.Size = new System.Drawing.Size(89, 13);
+            this.lblUserEmail.TabIndex = 1;
+            this.lblUserEmail.Text = "Username (Email)";
+            // 
+            // txtUserEmail
+            // 
+            this.txtUserEmail.Location = new System.Drawing.Point(114, 28);
+            this.txtUserEmail.Margin = new System.Windows.Forms.Padding(2);
+            this.txtUserEmail.Name = "txtUserEmail";
+            this.txtUserEmail.Size = new System.Drawing.Size(231, 20);
+            this.txtUserEmail.TabIndex = 0;
+            // 
+            // lblReturnMovies
+            // 
+            this.lblReturnMovies.AutoSize = true;
+            this.lblReturnMovies.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReturnMovies.Location = new System.Drawing.Point(208, 54);
+            this.lblReturnMovies.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblReturnMovies.Name = "lblReturnMovies";
+            this.lblReturnMovies.Size = new System.Drawing.Size(144, 24);
+            this.lblReturnMovies.TabIndex = 10;
+            this.lblReturnMovies.Text = "Return Movies";
+            this.lblReturnMovies.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.borrowMoviesToolStripMenuItem,
+            this.returnMoviesToolStripMenuItem,
+            this.manageUsersToolStripMenuItem,
+            this.manageMoviesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(597, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // borrowMoviesToolStripMenuItem
+            // 
+            this.borrowMoviesToolStripMenuItem.Name = "borrowMoviesToolStripMenuItem";
+            this.borrowMoviesToolStripMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.borrowMoviesToolStripMenuItem.Text = "Borrow Movies";
+            // 
+            // returnMoviesToolStripMenuItem
+            // 
+            this.returnMoviesToolStripMenuItem.Name = "returnMoviesToolStripMenuItem";
+            this.returnMoviesToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
+            this.returnMoviesToolStripMenuItem.Text = "Return Movies";
+            // 
+            // manageUsersToolStripMenuItem
+            // 
+            this.manageUsersToolStripMenuItem.Name = "manageUsersToolStripMenuItem";
+            this.manageUsersToolStripMenuItem.Size = new System.Drawing.Size(93, 20);
+            this.manageUsersToolStripMenuItem.Text = "Manage Users";
+            // 
+            // manageMoviesToolStripMenuItem
+            // 
+            this.manageMoviesToolStripMenuItem.Name = "manageMoviesToolStripMenuItem";
+            this.manageMoviesToolStripMenuItem.Size = new System.Drawing.Size(103, 20);
+            this.manageMoviesToolStripMenuItem.Text = "Manage Movies";
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Location = new System.Drawing.Point(199, 138);
+            this.btnReturn.Margin = new System.Windows.Forms.Padding(2);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(56, 21);
+            this.btnReturn.TabIndex = 7;
+            this.btnReturn.Text = "Return";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            // 
+            // FormReturnMovies
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(597, 482);
+            this.Controls.Add(this.grpBorrowedMovies);
+            this.Controls.Add(this.grpUserInformation);
+            this.Controls.Add(this.lblReturnMovies);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "FormReturnMovies";
             this.Text = "FormReturn";
+            this.grpBorrowedMovies.ResumeLayout(false);
+            this.grpUserInformation.ResumeLayout(false);
+            this.grpUserInformation.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.GroupBox grpBorrowedMovies;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox grpUserInformation;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblUserEmail;
+        private System.Windows.Forms.TextBox txtUserEmail;
+        private System.Windows.Forms.Label lblReturnMovies;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem borrowMoviesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem returnMoviesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageUsersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageMoviesToolStripMenuItem;
+        private System.Windows.Forms.Button btnReturn;
     }
 }
