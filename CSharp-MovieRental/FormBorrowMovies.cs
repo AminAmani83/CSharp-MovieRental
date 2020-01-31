a6af77feb66c9af37d2055649ee9a6c21f843634
patel6151825 <58268816+patel6151825@users.noreply.github.com>
2020-01-30 23:57:28 -0500

    updating and refreshing movie borrow list

--- CSharp-MovieRental/FormBorrowMovies.cs
+++ CSharp-MovieRental/FormBorrowMovies.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,7 +8,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using System.Web.UI.WebControls;
 using System.Windows.Forms;
 
 namespace CSharp_MovieRental
@@ -25,15 +24,24 @@ namespace CSharp_MovieRental
             base.OnLoad(e);
             context = new MovieRentalContext();
 
-            //Loading categories from DB
+            //Loading 
             context.Genres.Load();
-
             context.Users.Load();
+            context.Movies.Load();
 
             //bingding the data to the source
             this.genreBindingSource.DataSource = context.Genres.Local.ToBindingList();
 
             this.userBindingSource.DataSource = context.Users.Local.ToBindingList();
+
+            List<BorrowHistory> unAvailableBorrowHistoryList = context.BorrowHistories.Where(b => DateTime.Compare(b.ReturnDate, new DateTime(1910, 1, 1, 0, 0, 0)) < 0).ToList();
+            List<Movie> allMovieList = context.Movies.ToList();
+            foreach (BorrowHistory unAvailableBorrowHistory in unAvailableBorrowHistoryList)
+            {
+                int movieIndex = allMovieList.IndexOf(unAvailableBorrowHistory.Movie);
+                allMovieList.RemoveAt(movieIndex);
+            }
+            this.movieDataGridView.DataSource = allMovieList;
         }
 
         private void btnSearch_Click(object sender, EventArgs e)
@@ -43,16 +51,8 @@ namespace CSharp_MovieRental
         private void btnBorrow_Click(object sender, EventArgs e)
         {
             User borrowingUser = context.Users.Where(u => u.Email.Equals(emailTextBox.Text)).FirstOrDefault();
-            if (borrowingUser != null)
-            {
 
-            }
-
-            //int useId = (int)userDataGridView.CurrentRow.Cells[0].Value;
-            //int userId = borrowingUser.UserId;
-
-            int movieId = (int)movieDataGridView.CurrentRow.Cells[0].Value;
-            //lblUserId.Text = "userId:"+useId +" MovieId: "+movieId;
+            int movieId = (int)this.movieDataGridView.CurrentRow.Cells[0].Value;
 
             string connetionString = null;
             SqlConnection cnn;
@@ -62,34 +62,27 @@ namespace CSharp_MovieRental
 
             cnn = new SqlConnection(connetionString);
 
-
-            sql = "INSERT INTO BorrowHistories (BorrowDate, UserId, MovieId) VALUES (" + "'" + DateTime.Now + "'" + "," + borrowingUser.UserId + "," + movieId + ")";
-
-            try
+            if (borrowingUser == null)
             {
-                cnn.Open();
-                adapter.InsertCommand = new SqlCommand(sql, cnn);
-                adapter.InsertCommand.ExecuteNonQuery();
-                MessageBox.Show("Row inserted !! ");
+                MessageBox.Show("Please Enter Valid Email");
             }
-            catch (Exception ex)
+            else
             {
-                MessageBox.Show(ex.ToString());
+                sql = "INSERT INTO BorrowHistories (BorrowDate, UserId, MovieId) VALUES (" + "'" + DateTime.Now + "'" + "," + borrowingUser.UserId + "," + movieId + ")";
+                try
+                {
+                    cnn.Open();
+                    adapter.InsertCommand = new SqlCommand(sql, cnn);
+                    adapter.InsertCommand.ExecuteNonQuery();
+                    MessageBox.Show("Successfully Movie Borrowed! ");
+                    OnLoad(e);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
             }
-
-            /*
-            System.Data.SqlClient.SqlConnection sqlConnection1 =
-    new System.Data.SqlClient.SqlConnection("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=CSharp_MovieRental.MovieRentalContext;Integrated Security=True");
-
-            System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
-            cmd.CommandType = System.Data.CommandType.Text;
-            cmd.CommandText = "INSERT INTO BorrowHistories (BorrowDate, UserId, MovieId) VALUES ("+DateTime.UtcNow.Date+","+ useId+","+movieId+")";
-            cmd.Connection = sqlConnection1;
-
-            sqlConnection1.Open();
-            cmd.ExecuteNonQuery();
-            sqlConnection1.Close();
-            */
         }
         private void manageMoviesToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -123,7 +116,7 @@ namespace CSharp_MovieRental
 
         private void borrowMoviesToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            
             this.Hide();
             using (FormBorrowMovies borrowMovies = new FormBorrowMovies())
             {
